2faf894853a785912b4e0532af9a2f1f08a437f2
Sebastiaan Alvarez Rodriguez <sebastiaanalva@gmail.com>
2022-05-08 21:09:48 +0200

    WIP: rendering for compose

--- app/src/main/java/org/python/companion/support/RenderUtil.kt
+++ app/src/main/java/org/python/companion/support/RenderUtil.kt
@@ -2,30 +2,182 @@ package org.python.companion.support
 
 import android.content.Context
 import android.text.util.Linkify
+import android.util.TypedValue
+import android.view.View
+import android.widget.TextView
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.takeOrElse
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.material.snackbar.Snackbar
 import io.noties.markwon.Markwon
+import io.noties.markwon.ext.latex.JLatexMathPlugin
 import io.noties.markwon.ext.tables.TablePlugin
 import io.noties.markwon.ext.strikethrough.StrikethroughPlugin
 import io.noties.markwon.html.HtmlPlugin
+import io.noties.markwon.inlineparser.MarkwonInlineParserPlugin
 import io.noties.markwon.linkify.LinkifyPlugin
 import org.python.backend.data.datatype.RenderType
+import ru.noties.jlatexmath.JLatexMathDrawable
 
 object RenderUtil {
-    fun render(view: TextView, text: String, type: RenderType) {
-        if (type == NoteType.TYPE_NORMAL) {
-            view.setText(text, TextView.BufferType.SPANNABLE)
-            return
+
+    @Composable
+    fun RenderText(
+        text: String,
+        renderType: RenderType,
+        modifier: Modifier = Modifier,
+        color: Color = Color.Unspecified,
+        fontSize: TextUnit = TextUnit.Unspecified,
+        fontStyle: FontStyle? = null,
+        fontWeight: FontWeight? = null,
+        fontFamily: FontFamily? = null,
+        letterSpacing: TextUnit = TextUnit.Unspecified,
+        textDecoration: TextDecoration? = null,
+        textAlign: TextAlign? = null,
+        lineHeight: TextUnit = TextUnit.Unspecified,
+        overflow: TextOverflow = TextOverflow.Clip,
+        softWrap: Boolean = true,
+        maxLines: Int = Int.MAX_VALUE,
+        onTextLayout: (TextLayoutResult) -> Unit = {},
+        style: TextStyle = LocalTextStyle.current
+    ) {
+        when (renderType) {
+            RenderType.DEFAULT ->
+                Text(text = text, modifier = modifier, color, fontSize, fontStyle, fontWeight, fontFamily, letterSpacing, textDecoration, textAlign, lineHeight, overflow, softWrap, maxLines, onTextLayout, style)
+            RenderType.MARKDOWN ->
+                MarkdownText(text = text, modifier = modifier, useLatex = false, color, fontSize, fontStyle, fontWeight, fontFamily, letterSpacing, textDecoration, textAlign, lineHeight, overflow, softWrap, maxLines, onTextLayout, style)
+            RenderType.LATEX ->
+                MarkdownText(text = text, modifier = modifier, useLatex = true, color, fontSize, fontStyle, fontWeight, fontFamily, letterSpacing, textDecoration, textAlign, lineHeight, overflow, softWrap, maxLines, onTextLayout, style)
+
         }
-        val renderer: Markwon = when (type) {
-            RenderType.MARKDOWN -> getMDRenderer(view.getContext())
-            RenderType.LATEX -> getLatexMDRenderer(
-                view,
-                view.getContext(),
-                view.getTextSize()
+    }
+    @Composable
+    fun MarkdownText(
+        text: String,
+        modifier: Modifier = Modifier,
+        useLatex: Boolean = false,
+        color: Color = Color.Unspecified,
+        fontSize: TextUnit = TextUnit.Unspecified,
+        fontStyle: FontStyle? = null,
+        fontWeight: FontWeight? = null,
+        fontFamily: FontFamily? = null,
+        letterSpacing: TextUnit = TextUnit.Unspecified,
+        textDecoration: TextDecoration? = null,
+        textAlign: TextAlign? = null,
+        lineHeight: TextUnit = TextUnit.Unspecified,
+        overflow: TextOverflow = TextOverflow.Clip,
+        softWrap: Boolean = true,
+        maxLines: Int = Int.MAX_VALUE,
+        onTextLayout: (TextLayoutResult) -> Unit = {},
+        style: TextStyle = LocalTextStyle.current,
+        onClick: (() -> Unit)? = null,
+        // this option will disable all clicks on links, inside the markdown text
+        // it also enable the parent view to receive the click event
+        disableLinkMovementMethod: Boolean = false,
+    ) {
+        val defaultColor: Color = LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
+        val context: Context = LocalContext.current
+        val markdownRender: Markwon = remember { createMarkdownRender(context) }
+        AndroidView(
+            modifier = modifier,
+            factory = { ctx ->
+                createTextView(
+                    context = ctx,
+                    color = color,
+                    defaultColor = defaultColor,
+                    fontSize = fontSize,
+                    maxLines = maxLines,
+                    style = style,
+                    textAlign = textAlign,
+                    onClick = onClick,
+                )
+            },
+            update = { textView ->
+                markdownRender.setMarkdown(textView, text)
+                if (disableLinkMovementMethod) {
+                    textView.movementMethod = null
+                }
+            }
+        )
+    }
+
+    private fun createTextView(
+        context: Context,
+        color: Color = Color.Unspecified,
+        defaultColor: Color,
+        fontSize: TextUnit = TextUnit.Unspecified,
+        textAlign: TextAlign? = null,
+        maxLines: Int = Int.MAX_VALUE,
+        style: TextStyle,
+        onClick: (() -> Unit)? = null
+    ): TextView {
+        val textColor = color.takeOrElse { style.color.takeOrElse { defaultColor } }
+        val mergedStyle = style.merge(
+            TextStyle(
+                color = textColor,
+                fontSize = fontSize,
+                textAlign = textAlign,
             )
-            else -> getLatexMDRenderer(view, view.getContext(), view.getTextSize())
+        )
+        return TextView(context).apply {
+            onClick?.let { setOnClickListener { onClick() } }
+            setTextColor(textColor.toArgb())
+            setMaxLines(maxLines)
+            setTextSize(TypedValue.COMPLEX_UNIT_DIP, mergedStyle.fontSize.value)
+
+            textAlign?.let { align ->
+                textAlignment = when (align) {
+                    TextAlign.Left, TextAlign.Start -> View.TEXT_ALIGNMENT_TEXT_START
+                    TextAlign.Right, TextAlign.End -> View.TEXT_ALIGNMENT_TEXT_END
+                    TextAlign.Center -> View.TEXT_ALIGNMENT_CENTER
+                    else -> View.TEXT_ALIGNMENT_TEXT_START
+                }
+            }
         }
-        renderer.setMarkdown(view, text)
+    }
+
+    private fun createMarkdownRender(context: Context, withLatex: Boolean = false): Markwon {
+        val builder = getStandardMDRenderer(context)
+        if (withLatex)
+            builder
+                .usePlugin(MarkwonInlineParserPlugin.create())
+                .usePlugin(JLatexMathPlugin.create(textSize) { builder ->
+                    builder.inlinesEnabled(true)
+                    builder.errorHandler { latex, error ->
+                        // Receive error and optionally return drawable to be displayed instead
+                        Snackbar.make(
+                            view,
+                            "Latex error: " + error.getLocalizedMessage(),
+                            Snackbar.LENGTH_INDEFINITE
+                        ).show()
+                        null
+                    }
+                    builder.executorService(Executors.newCachedThreadPool())
+
+                    //Theme stuff
+                    builder.theme().backgroundProvider { ColorDrawable(0) }
+                    builder.theme().blockFitCanvas(true)
+                    builder.theme().blockHorizontalAlignment(JLatexMathDrawable.ALIGN_CENTER)
+                })
+        return builder.build()
     }
 
     private fun getStandardMDRenderer(context: Context): Markwon.Builder {
@@ -34,40 +186,5 @@ object RenderUtil {
             .usePlugin(StrikethroughPlugin.create())
             .usePlugin(TablePlugin.create(context))
             .usePlugin(LinkifyPlugin.create(Linkify.WEB_URLS))
-    }
-
-    private fun getStandardLatexMDRenderer(
-        view: View,
-        context: Context,
-        textSize: Float
-    ): Markwon.Builder {
-        return getStandardMDRenderer(context)
-            .usePlugin(MarkwonInlineParserPlugin.create())
-            .usePlugin(JLatexMathPlugin.create(textSize) { builder ->
-                builder.inlinesEnabled(true)
-                builder.errorHandler { latex, error ->
-                    // Receive error and optionally return drawable to be displayed instead
-                    Snackbar.make(
-                        view,
-                        "Latex error: " + error.getLocalizedMessage(),
-                        Snackbar.LENGTH_INDEFINITE
-                    ).show()
-                    null
-                }
-                builder.executorService(Executors.newCachedThreadPool())
-
-                //Theme stuff
-                builder.theme().backgroundProvider { ColorDrawable(0) }
-                builder.theme().blockFitCanvas(true)
-                builder.theme().blockHorizontalAlignment(JLatexMathDrawable.ALIGN_CENTER)
-            })
-    }
-
-    private fun getMDRenderer(context: Context): Markwon {
-        return getStandardMDRenderer(context).build()
-    }
-
-    private fun getLatexMDRenderer(view: View, context: Context, textSize: Float): Markwon {
-        return getStandardLatexMDRenderer(view, context, textSize).build()
     }
 }
